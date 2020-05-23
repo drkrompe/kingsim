b9e1a1143ef06966f4a37e2e22f77edc9e43bb4a
drkrompe <kromp08@gmail.com>
2020-05-23 12:41:18 -0400

    cleanup and query work

--- src/intermediatejobexecutors/IntermediateJobExecutors.js
+++
@@ -1,9 +0,0 @@
-import MoveToExecution from "./moveto/MoveToExecution";
-
-export default class IntermediateJobExecutors {
-    static lookupTable = new Map(['MoveTo', MoveToExecution]);
-
-    static lookup = (intermediateJobType = "") => {
-        return this.lookupTable.get(intermediateJobType);
-    }
-}
---
+++ src/kingsim/systems/querysystem/QuerySystem.js
@@ -0,0 +1,12 @@
+import System from "../../../ecs/systems/System";
+
+export default class QuerySystem extends System {
+
+    // Component Data Used
+    // - ItemQueryComponent
+    //   - itemQuery null | ItemQuery object
+
+    systemTick(timeDelta) {
+        
+    }
+}
